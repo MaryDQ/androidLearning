c8bbf5621f5cfe78dd069ecfba1db02d78d66944
MarkLing <964573692@qq.com>
2019-08-19 06:57:41 +0800

    1.CameraX实现预览

---
+++ app/src/main/java/com/mlx/administrator/myapplication/camera_x/CameraXActivity.kt
@@ -0,0 +1,132 @@
+package com.mlx.administrator.myapplication.camera_x
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.mlx.administrator.myapplication.R
+
+// This is an arbitrary number we are using to keep tab of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class CameraXActivity : AppCompatActivity() {
+
+    // Add this after onCreate
+
+    private lateinit var viewFinder: TextureView
+
+    private fun startCamera() {
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1, 1))
+            setTargetResolution(Size(640, 640))
+        }.build()
+
+        //Build the viewfinder user case
+        val preview = Preview(previewConfig)
+
+        //Every time the viewfinder is updated,recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+            //To update the SurfaceTexture,we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        //Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        //Correct preview output to account for display rotation
+        val rotationDegrees = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 0
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        viewFinder.setTransform(matrix)
+    }
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_x)
+
+        // Add this at the end of onCreate function
+
+        viewFinder = findViewById(R.id.view_finder)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Every time the provided texture view changes, recompute layout
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+}
--- app/src/test/java/com/mlx/administrator/myapplication/Test.java
+++ app/src/test/java/com/mlx/administrator/myapplication/Test.java
@@ -1,13 +1,27 @@
 package com.mlx.administrator.myapplication;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Test {
     int a=1000,b=600;
     @org.junit.Test
     public void main(){
 
-        swap(a,b);
-        System.out.println("a:"+a+",b:"+b);
+        testReverse();
     }
+
+    private void testReverse() {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(Arrays.toString(tempString));
+        System.out.println(System.currentTimeMillis());
+        Collections.reverse(Arrays.asList(tempString));
+        System.out.println(System.currentTimeMillis());
+        System.out.println(Arrays.toString(tempString));
+        System.out.println(System.currentTimeMillis());
+    }
+
+    String[] tempString={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
 
 
     private void swap(int a,int b ){
