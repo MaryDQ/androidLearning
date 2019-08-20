ad421e032df9f4c365515d2ccac10ce11f5880ed
MarkLing <964573692@qq.com>
2019-08-21 07:25:14 +0800

    1.CameraX实现预览,并且可以拍照，分析每一秒图片的亮度

--- app/src/main/java/com/mlx/administrator/myapplication/camera_x/CameraXActivity.kt
+++ app/src/main/java/com/mlx/administrator/myapplication/camera_x/CameraXActivity.kt
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager
 import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.util.Rational
 import android.util.Size
@@ -18,6 +20,8 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.mlx.administrator.myapplication.R
 import java.io.File
+import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
 
 // This is an arbitrary number we are using to keep tab of the permission
 // request. Where an app has multiple context for requesting permission,
@@ -84,8 +88,24 @@ class CameraXActivity : AppCompatActivity() {
             })
         }
 
+        //Setup image analysis pipeline that computes average pixel luminance
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            //Use a work thread for image analysis to prevent glitches
+            val analyzerThread = HandlerThread("LuminosityAnalysis")
+                .apply { start() }
+            setCallbackHandler(Handler(analyzerThread.looper))
+            //In out analysis, we care more about the latest image than
+            //analyzing *every* image
+            setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
 
-        CameraX.bindToLifecycle(this, preview, imageCapture)
+        //Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            analyzer=LumionsitAnalyzer()
+        }
+
+
+        CameraX.bindToLifecycle(this, preview, imageCapture,analyzerUseCase)
     }
 
     private fun updateTransform() {
@@ -160,5 +180,40 @@ class CameraXActivity : AppCompatActivity() {
         viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
             updateTransform()
         }
+    }
+
+    private class LumionsitAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimeStamp = 0L
+        /**
+         * Helper extension function used to extract a byte array from an
+         * image plane buffer
+         */
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    //Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   //Copy the buffer into a byte array
+            return data //Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+            val currentTimeStamp = System.currentTimeMillis()
+            //Calculate the average luma no more often than every second
+            if (currentTimeStamp-lastAnalyzedTimeStamp>=
+                    TimeUnit.SECONDS.toMillis(1)){
+                //Since format in ImageAnalysis is YUV, image.planes[0]
+                //contains the Y (luminance) plane
+                val buffer=image!!.planes[0].buffer
+                //Extract image data from callback object
+                val data=buffer.toByteArray()
+                //Convert the data into an array of pixel values
+                val pixels =data.map { it.toInt() and 0xFF }
+                //Compute average luminance for the image
+                val lum=pixels.average()
+                //Log the new luma value
+                Log.d("CameraXActivity","Average luminosity: $lum")
+                lastAnalyzedTimeStamp=currentTimeStamp
+            }
+        }
+
     }
 }
