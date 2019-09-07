baa9eaa608dfb00456eefdbe4d5aa8f3f385d1f7
MarkLing <964573692@qq.com>
2019-09-08 07:06:47 +0800

    1.准备 编写NFC

---
+++ app/src/main/java/com/mlx/administrator/myapplication/nfc/NfcActivity.kt
@@ -0,0 +1,13 @@
+package com.mlx.administrator.myapplication.nfc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mlx.administrator.myapplication.R
+
+class NfcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
+    }
+}
